9011f03853666dba968f8fbe84eefb6437b951ce
akheidar <50970184+akheidar@users.noreply.github.com>
2019-11-25 12:44:11 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# akheidar.github.io
+# Delta 311 App
+
+## A public aquatic-related reporting system for the California Division of Boating and Waterways
---
+++ animations.js
@@ -0,0 +1,95 @@
+//JS code for managing user interactions for prototype
+//Content grabbers
+var bg = document.querySelector(".mainframe");
+var reportBtn = document.querySelector(".report-btn");
+var header = document.querySelector(".header");
+var welcomeScreen = [header];
+var questions = document.querySelector(".container-questions");
+var progressBar = document.querySelector(".progress-bar");
+var review = document.querySelector(".container-review");
+
+//functions
+//content grabber for question q
+var question = function(q) {
+  return document.querySelector(".container-questions .questions " + q);
+}
+
+//next button for the question q
+var nextQ = function(q) {
+  return document.querySelector(".container-questions .questions " + q + " .next-btn");
+}
+
+//function which manages the transition between two questions
+function qTransition(q_before, q_after) {
+  q_before.style.animation = "questionsTransitionOut 0.5s ease-in forwards"
+  setTimeout(function() {q_before.style.display = "none";}, 800);
+  q_after.style.animation = "questionsTransitionIn 0.5s 1.0s forwards";
+  q_after.style.display = "block";
+}
+
+//function which manages the growth of the progress bar
+function progressTransition(stage) {
+  if(stage === 0) {
+    progressBar.style.animation = "progressAppear 0.5s forwards";
+  } else {
+    progressBar.style.animation = "progressInc" + stage + " 0.5s forwards";
+  }
+}
+
+
+//Stuff to do when user clicks the New Report button
+reportBtn.addEventListener('click', function() {
+  (function() {
+    //apply the screenTransition keyframe animation to each element in the login screen
+    welcomeScreen.forEach(function(el) {
+      el.style.animation = "screenTransition 1.0s forwards";
+    });
+    //overwrite the animation of the login button
+    reportBtn.style.animationName = "none";
+    reportBtn.style.animation = "screenTransition 1.0s forwards";
+  })();
+
+  //When user clicks the login button, scale the overlay to make room for the options menu
+  //Also stop the wave animation
+  setTimeout(function() {
+    var overlay = document.querySelector(".container-welcome");
+    overlay.style.animation = "moveWater 0.5s forwards";
+    bg.style.animation = "changeBG1 0.5s forwards";
+    questions.style.animation = "questionsTransitionIn 0.5s 0.5s forwards";
+    progressBar.style.display = "block";
+    progressTransition(0);
+  }, 200);
+});
+
+//code for going from question 1 to question 2
+nextQ(".q1").addEventListener("click", function() {
+  qTransition(question(".q1"), question(".q2"));
+  progressTransition(1);
+});
+
+//code for going from question 2 to question 3
+nextQ(".q2").addEventListener("click", function() {
+  qTransition(question(".q2"), question(".q3"));
+  progressTransition(2);
+});
+
+//code for going from completed questions to form submission review
+nextQ(".q3").addEventListener("click", function() {
+  var micro = document.querySelector(".container-questions .micro-copy");
+  micro.style.animation = "questionsTransitionOut 0.3s ease-out forwards";
+  bg.style.animation = "changeBG2 0.5s 0.2s forwards";
+  question(".q3").style.animation = "questionsTransitionOut 0.5s ease-in forwards";
+  progressTransition(3);
+  setTimeout(function() {
+    review.style.display = "block";
+  }, 500);
+});
+
+
+//Get the refresh button
+var refresh = document.querySelector(".refresh-btn");
+
+//Reload the prototype when user clicks the reload button
+refresh.addEventListener('click', function() {
+  location.reload(true);
+});
